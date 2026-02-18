9f04b13e5bd5377e36573789eb97e7b5f140c88c
Priyanshu Dubey <37262690+priyanshudubey@users.noreply.github.com>
2026-02-18 20:12:03 +0000

    Fix: Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,51 @@
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const resume = require('./resume.json');
 
-"use strict";
-
-var inquirer = require("inquirer");
-var chalk = require("chalk");
-
-var response = chalk.bold.yellow;
-
-var resume = require("./resume.json");
-
-var resumePrompts = {
-    type: "list",
-    name: "resumeOptions",
-    message: "What do you want to know about me?",
-    choices: [...Object.keys(resume), "Exit"]
-};
-
-function main() {
-    console.log("----------------------------------------------------------");
-    console.log("----------------------------------------------------------");
-    console.log("Hello, My name is Priyanshu Dubey.Welcome to my CLI Resume");
-    console.log("----------------------------------------------------------");
-    console.log("----------------------------------------------------------");
-    resumeHandler();
+function response(msg) {
+    return chalk.green(msg);
 }
 
 function resumeHandler() {
+    const resumePrompts = {
+        type: "list",
+        name: "resumeOptions",
+        message: "What do you want to know about me?",
+        choices: Object.keys(resume).concat("Exit")
+    };
+
     inquirer.prompt(resumePrompts).then(answer => {
-        if (answer.resumeOptions == "Exit") {
+        if (answer.resumeOptions === "Exit") {
+            console.log(chalk.yellow("Thank you for using the CLI Resume!"));
             return;
         }
-        var option = answer.resumeOptions;
+        const option = answer.resumeOptions;
         console.log(response("--------------------------------------"));
-        resume[`${option}`].forEach(info => {
+        resume[option].forEach(info => {
             console.log(response("|   => " + info));
         });
         console.log(response("--------------------------------------"));
-        inquirer
-            .prompt({
-                type: "list",
-                name: "exitBack",
-                message: "Go back or Exit?",
-                choices: ["Back", "Exit"]
-            })
-            .then(choice => {
-                if (choice.exitBack == "Back") {
-                    resumeHandler();
-                } else {
-                    return;
-                }
-            });
-    });
+        inquirer.prompt({
+            type: "list",
+            name: "exitBack",
+            message: "Go back or Exit?",
+            choices: ["Back", "Exit"]
+        }).then(choice => {
+            if (choice.exitBack === "Back") {
+                resumeHandler();
+            } else {
+                console.log(chalk.yellow("Thank you for using the CLI Resume!"));
+                return;
+            }
+        }).catch(err => console.error(chalk.red("Failed to get choice: " + err)));
+    }).catch(err => console.error(chalk.red("Failed to get answer: " + err)));
+}
+
+function main() {
+    console.log(chalk.blue("----------------------------------------------------------"));
+    console.log(chalk.blue("Hello, My name is Priyanshu Dubey. Welcome to my CLI Resume"));
+    console.log(chalk.blue("----------------------------------------------------------"));
+    resumeHandler();
 }
 
 main();
