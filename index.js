fb05632e3980540075fd7818011e5108a7976f5a
Priyanshu Dubey <37262690+priyanshudubey@users.noreply.github.com>
2026-02-18 20:27:50 +0000

    Fix: Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,54 @@
 
-"use strict";
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const resume = require('./resume.json');
 
-var inquirer = require("inquirer");
-var chalk = require("chalk");
-
-var response = chalk.bold.yellow;
-
-var resume = require("./resume.json");
-
-var resumePrompts = {
-    type: "list",
-    name: "resumeOptions",
-    message: "What do you want to know about me?",
-    choices: [...Object.keys(resume), "Exit"]
-};
-
-function main() {
-    console.log("----------------------------------------------------------");
-    console.log("----------------------------------------------------------");
-    console.log("Hello, My name is Priyanshu Dubey.Welcome to my CLI Resume");
-    console.log("----------------------------------------------------------");
-    console.log("----------------------------------------------------------");
-    resumeHandler();
-}
-
+// Define a resume handler to handle user inputs
 function resumeHandler() {
     inquirer.prompt(resumePrompts).then(answer => {
-        if (answer.resumeOptions == "Exit") {
+        if (answer.resumeOptions === "Exit") {
             return;
         }
-        var option = answer.resumeOptions;
-        console.log(response("--------------------------------------"));
-        resume[`${option}`].forEach(info => {
-            console.log(response("|   => " + info));
-        });
-        console.log(response("--------------------------------------"));
-        inquirer
-            .prompt({
-                type: "list",
-                name: "exitBack",
-                message: "Go back or Exit?",
-                choices: ["Back", "Exit"]
-            })
-            .then(choice => {
-                if (choice.exitBack == "Back") {
-                    resumeHandler();
-                } else {
-                    return;
-                }
+
+        const option = answer.resumeOptions;
+        console.log(chalk.blue("--------------------------------------"));
+        if (resume[option]) {
+            resume[option].forEach(info => {
+                console.log(chalk.green("|   => " + info));
             });
+        } else {
+            console.log(chalk.red("No information available for this option."));
+        }
+        console.log(chalk.blue("--------------------------------------"));
+
+        promptToGoBack();
+    }).catch(error => {
+        console.error(chalk.red("An error occurred during the prompt: ", error.message));
     });
 }
 
-main();
+// Function to prompt the user to go back or exit
+function promptToGoBack() {
+    inquirer.prompt({
+        type: "list",
+        name: "exitBack",
+        message: "Go back or Exit?",
+        choices: ["Back", "Exit"]
+    }).then(choice => {
+        if (choice.exitBack === "Back") {
+            resumeHandler();
+        }
+    }).catch(error => {
+        console.error(chalk.red("An error occurred during the prompt: ", error.message));
+    });
+}
+
+// Main function to initialize the program
+function main() {
+    console.log(chalk.yellow("----------------------------------------------------------"));
+    console.log(chalk.yellow("Hello, My name is Priyanshu Dubey. Welcome to my CLI Resume"));
+    console.log(chalk.yellow("----------------------------------------------------------"));
+    resumeHandler(); // Start the resume handler
+}
+
+main(); // Invoke main to start the application
