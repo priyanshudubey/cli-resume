34bca2ddda269476327aa2d7d57178b59b4a68af
Priyanshu Dubey <37262690+priyanshudubey@users.noreply.github.com>
2026-02-18 15:53:08 +0000

    Fix: Update generated-code.ts

---
+++ src/generated-code.ts
@@ -0,0 +1,68 @@
+[object Object], inquirer = ,[object Object],(,[object Object],);
+,[object Object], resume = ,[object Object],(,[object Object],);
+,[object Object], chalk = ,[object Object],(,[object Object],);
+
+,[object Object], ,[object Object],(,[object Object],) {
+    ,[object Object],.,[object Object],(chalk.,[object Object],(,[object Object],));
+    resume[,[object Object],].,[object Object],(,[object Object], {
+        ,[object Object],.,[object Object],(chalk.,[object Object],(,[object Object], + info));
+    });
+    ,[object Object],.,[object Object],(chalk.,[object Object],(,[object Object],));
+}
+
+,[object Object], ,[object Object],(,[object Object],) {
+    ,[object Object], inquirer.,[object Object],({
+        ,[object Object],: ,[object Object],,
+        ,[object Object],: ,[object Object],,
+        ,[object Object],: ,[object Object],,
+        ,[object Object],: [,[object Object],, ,[object Object],]
+    });
+}
+
+,[object Object], ,[object Object],(,[object Object],) {
+    ,[object Object], resumePrompts = {
+        ,[object Object],: ,[object Object],,
+        ,[object Object],: ,[object Object],,
+        ,[object Object],: ,[object Object],,
+        ,[object Object],: [
+            ,[object Object],,
+            ,[object Object],,
+            ,[object Object],,
+            ,[object Object],,
+            ,[object Object],,
+            ,[object Object],,
+            ,[object Object],,
+            ,[object Object],
+        ]
+    };
+
+    inquirer.,[object Object],(resumePrompts)
+        .,[object Object],(,[object Object], {
+            ,[object Object], (answer.,[object Object], === ,[object Object],) {
+                ,[object Object],.,[object Object],(chalk.,[object Object],(,[object Object],));
+                process.,[object Object],(,[object Object],); ,[object Object],
+            } ,[object Object], {
+                ,[object Object],(answer.,[object Object],);
+                ,[object Object], ,[object Object],();
+            }
+        })
+        .,[object Object],(,[object Object], {
+            ,[object Object], (choice && choice.,[object Object], === ,[object Object],) {
+                ,[object Object],(); ,[object Object],
+            } ,[object Object], {
+                ,[object Object],.,[object Object],(chalk.,[object Object],(,[object Object],));
+                process.,[object Object],(,[object Object],); ,[object Object],
+            }
+        })
+        .,[object Object],(,[object Object], {
+            ,[object Object],.,[object Object],(chalk.,[object Object],(,[object Object],, error));
+            process.,[object Object],(,[object Object],); ,[object Object],
+        });
+}
+
+,[object Object], ,[object Object],(,[object Object],) {
+    ,[object Object],.,[object Object],(chalk.,[object Object],(,[object Object],));
+    ,[object Object],();
+}
+
+,[object Object],();
